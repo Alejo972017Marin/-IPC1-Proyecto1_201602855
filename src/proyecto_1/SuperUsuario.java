a3b4f2ba93c03be003a49fbb12935bf2372726bf
Alejo972017Marin <alejandrograndemarin@hotmail.com>
2017-09-04 21:28:46 -0600

    Actualizado.

--- src/proyecto_1/SuperUsuario.java
+++ src/proyecto_1/SuperUsuario.java
@@ -3,9 +3,9 @@ package proyecto_1;
 
 public class SuperUsuario {
     
-    public boolean SuperUsuario(String User, String Pas){
+    public boolean SuperUsuario(String User){
         
-        return User.equals("IPC1") && Pas.equals("IPC1");
+        return User.equals("IPC1") ;
         
         
     }
