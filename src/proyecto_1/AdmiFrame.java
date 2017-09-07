ddc13d5c86c873c79f0a161db45b4082c31b8e15
Alejo972017Marin <alejandrograndemarin@hotmail.com>
2017-09-06 21:50:05 -0600

    Actualizado.

--- src/proyecto_1/AdmiFrame.java
+++ src/proyecto_1/AdmiFrame.java
@@ -6,11 +6,15 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JScrollPane;
 import javax.swing.JTable;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 
+
 public class AdmiFrame extends JFrame {
+
+
 
     private JLabel lbUsuario;
     private JLabel lbDeposito;
@@ -20,18 +24,22 @@ public class AdmiFrame extends JFrame {
     private JButton btBorrar;
     private JTable tbtTabla;
     private JComboBox cbEmpresa;
-    Object tab[];
-    private Object Datoss[][]={{"No","Nombre","Deposito","Empresa"},{" ","","",""},{" ","","",""},{" ","","",""},{" ","","",""},{" ","","",""},{" ","","",""},{" ","","",""},{" ","","",""},{" ","","",""}};
-    private String Cab[] ={"No","Nombre","Deposito","Empresa"};
-    
-    private String Cash[] ={"Pro-Pisto", "Cahs-Money"};
-    
-
+ 
+    int Codigo,sueldo;
+    String Nombre,empresa;
+    private Object datoss[][]={{"Codigo", "Nombre", "Deposito", "Empresa"}};
+    private Object cab[]={"Codigo", "Nombre", "Deposito", "Empresa"};   
+ 
+    private String Cash[] = {"Pro-Pisto", "Cahs-Money"};
 
     public AdmiFrame() {
 
-        setSize(600, 600);
+            
+                setSize(600, 600);
         setTitle("Administracion");
+        JScrollPane sp = new JScrollPane(tbtTabla);
+        getContentPane().add(sp);
+
         setLayout(null);
         setDefaultCloseOperation(Login.EXIT_ON_CLOSE);
         setResizable(true);
@@ -43,7 +51,7 @@ public class AdmiFrame extends JFrame {
         btAgregar = new JButton("Agregar");
         btBorrar = new JButton("Borrar");
         cbEmpresa = new JComboBox(Cash);
-        tbtTabla = new JTable(Datoss,Cab);
+        tbtTabla = new JTable(datoss,cab);
 
         add(lbUsuario);
         add(txUsuario);
@@ -53,24 +61,23 @@ public class AdmiFrame extends JFrame {
         add(btBorrar);
         add(tbtTabla);
         add(cbEmpresa);
-        
+
         btAgregar.setHorizontalTextPosition(SwingConstants.CENTER);
         btAgregar.setVerticalTextPosition(SwingConstants.BOTTOM);
 
         btBorrar.setHorizontalTextPosition(SwingConstants.CENTER);
         btBorrar.setVerticalTextPosition(SwingConstants.BOTTOM);
 
-        
         lbUsuario.reshape(20, 20, 100, 30);
         txUsuario.reshape(100, 20, 100, 30);
-        lbDeposito.reshape(20, 80,100, 30);
+        lbDeposito.reshape(20, 80, 100, 30);
         txDeposito.reshape(100, 80, 100, 30);
         btAgregar.reshape(20, 150, 100, 50);
         btBorrar.reshape(120, 150, 100, 50);
         tbtTabla.reshape(250, 80, 300, 400);
         cbEmpresa.reshape(260, 20, 100, 30);
-        
-                btAgregar.addActionListener(new ActionListener() {
+
+        btAgregar.addActionListener(new ActionListener() {
 
             @Override
             public void actionPerformed(ActionEvent evt) {
@@ -78,24 +85,18 @@ public class AdmiFrame extends JFrame {
             }
 
             private void AgregarActionPerformed(ActionEvent evt) {
-                int cont=0;
+                int cont = 0;
                 String nombre = txUsuario.getText();
                 String Deposito = txUsuario.getText();
                 String empresa = cbEmpresa.getToolTipText();
-                    Object acom[]={ cont++,nombre,Deposito,empresa};
-                
-                    
-                
+                Object acom[] = {cont++, nombre, Deposito, empresa};
 
             }
-            
 
         });
-                
-           
-        
-    }
-    
 
+    }
+
+   
 
 }
